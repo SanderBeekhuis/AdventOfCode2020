4e14eedd30c74437ddb17c682a025ac89b187485
Sander Beekhuis <sbeekhuis@enrise.com>
2020-12-04 16:31:12 +0100

    Day 2 Part 1

---
+++ day2/main.py
@@ -0,0 +1,22 @@
+from collections import Counter
+
+def is_valid(line):
+  line = line.strip()
+  policy, password = line.split(': ')
+  count, letter = policy.split(' ')
+  min_count, max_count = map(int, count.split('-'))
+
+  # Count password characters
+  c = Counter(password)
+
+  return c[letter] in range(min_count, max_count+1)
+
+
+lines = open('input.txt', 'r').readlines()
+valid_password_count = 0
+
+for line in lines:
+  if is_valid(line):
+    valid_password_count += 1
+
+print(valid_password_count)
---
+++ day2/test.py
@@ -0,0 +1,13 @@
+import main
+import pytest
+
+@pytest.mark.parametrize(
+  "test,expected",
+  [
+    ("1-3 a: abcde", True),
+    ("1-3 b: cdefg", False),
+    ("2-9 c: ccccccccc", True),
+  ]
+)
+def test_is_valid(test, expected):
+  assert main.is_valid(test) == expected
