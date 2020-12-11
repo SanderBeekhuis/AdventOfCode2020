a6015f8f13a7582ceb802e3cca24228683e7a30f
Sander Beekhuis <sbeekhuis@enrise.com>
2020-12-11 15:41:53 +0100

    Part2

--- day4/main.py
+++ day4/main.py
@@ -1,5 +1,6 @@
 
 import more_itertools
+import re
 
 def run(file: str='input.txt') -> int:
   lines = open(file, 'r').readlines()
@@ -23,9 +24,32 @@ def parse(lines):
   passport = more_itertools.flatten(passport)
   return {k: v for k, v in map(lambda s: s.split(':'), passport) }
 
-def check(passport: dict) -> bool:
-  return {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}.issubset(passport.keys())
+def check(p: dict) -> bool:
+  return bool(
+    {'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'}.issubset(p.keys()) and
+    1920 <= int(p['byr']) <= 2002 and
+    2010 <= int(p['iyr']) <= 2020 and
+    2020 <= int(p['eyr']) <= 2030 and
+    height_check(p['hgt']) and
+    hair_color_check(p['hcl']) and
+    p['ecl'] in {'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'} and
+    re.fullmatch('[0-9]{9}', p['pid'])
+  )
 
+
+def height_check(h):
+  height = int(h[:-2])
+  unit = h[-2:]
+
+  if unit == 'cm':
+    return 150 <= height <= 193
+  elif unit == 'in':
+    return 59 <= height <=76
+  else:
+    return False
+
+def hair_color_check(c):
+  return re.fullmatch('#[0-9a-f]{6}', c)
 
 if __name__ == "__main__":
   print(run())
--- day4/test.py
+++ day4/test.py
@@ -3,3 +3,6 @@ import pytest
 
 def test_run():
   assert main.run(file='test_input.txt') == 2
+
+def test_run_invalid():
+  assert main.run(file='input_invalid.txt') == 0
