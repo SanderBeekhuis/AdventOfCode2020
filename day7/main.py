8df53e7ade98a44fd0fffd5411d2aab7f1b62fbe
Sander Beekhuis <sbeekhuis@enrise.com>
2020-12-16 16:32:42 +0100

    Day, part2

--- day7/main.py
+++ day7/main.py
@@ -1,9 +1,20 @@
+import collections
+
 def run(file: str='day7/input.txt'):
   lines = open(file, 'r').readlines()
   lines = map(lambda l: l.strip(), lines)
 
-  bag_contents = {bag: content for bag, content in map(parse_line, lines)}
+  return {bag: content for bag, content in map(parse_line, lines)}
 
+def parse_line(line):
+  bag, raw_contents = line.split('s contain ')
+  if raw_contents == 'no other bags.':
+    return bag, {}
+  raw_contents = raw_contents.split(', ')
+  contents = {c[2:].rstrip('s.'): int(c[0]) for c in raw_contents}
+  return (bag, contents)
+
+def bags_possibly_containing_gold_bag(bag_contents):
   bags_possibly_containing_gold_bag = {'shiny gold bag'}
   size = 0
 
@@ -13,19 +24,29 @@ def run(file: str='day7/input.txt'):
 
     # Add bags that contain (bags that) can contain the shiny gold bag
     for bag, contents in bag_contents.items():
-      if contents.intersection(bags_possibly_containing_gold_bag):
+      if set(contents.keys()).intersection(bags_possibly_containing_gold_bag):
         bags_possibly_containing_gold_bag.add(bag)
 
   # Subtract one for the shiny gold bag itself
   return len(bags_possibly_containing_gold_bag) - 1
 
-def parse_line(line):
-  bag, raw_contents = line.split('s contain ')
-  if raw_contents == 'no other bags.':
-    return bag, set()
-  raw_contents = raw_contents.split(', ')
-  contents = {c[2:].rstrip('s.') for c in raw_contents}
-  return (bag, contents)
+def bags_inside_gold_bag(bag_contents):
+  count = 0
+  current_bags = {'shiny gold bag': 1}
+
+  while current_bags:
+    new_bags = collections.Counter()
+    for bag, occurrence in current_bags.items():
+        for inner_bag, included in bag_contents[bag].items():
+          new_bags[inner_bag] += occurrence * included
+
+    count+=sum(new_bags.values())
+    current_bags = new_bags
+    print(count)
+
+  return count
 
 if __name__ == "__main__":
-  print("Number of possible outermost bags: ", run())
+  bag_contents=run()
+  print("Number of possible outermost bags: ", bags_possibly_containing_gold_bag(bag_contents))
+  print("Number of bags inside a gold bag: ", bags_inside_gold_bag(bag_contents))
--- day7/test_main.py
+++ day7/test_main.py
@@ -4,14 +4,15 @@ import day7.main
 
 @pytest.mark.parametrize('line,expected',
   [
-    ('light red bags contain 1 bright white bag, 2 muted yellow bags.', ('light red bag', {'bright white bag', 'muted yellow bag'})),
-    ('bright white bags contain 1 shiny gold bag.', ('bright white bag', {'shiny gold bag'})),
-    ('dotted black bags contain no other bags.', ('dotted black bag', set())),
+    ('light red bags contain 1 bright white bag, 2 muted yellow bags.', ('light red bag', {'bright white bag': 1, 'muted yellow bag': 2})),
+    ('bright white bags contain 1 shiny gold bag.', ('bright white bag', {'shiny gold bag': 1})),
+    ('dotted black bags contain no other bags.', ('dotted black bag', {})),
   ]
 )
 def test_parse_line(line, expected):
   assert expected == day7.main.parse_line(line)
 
-
 def test_run():
-  assert 4 == day7.main.run('day7/test_input.txt')
+  bag_contents = day7.main.run('day7/test_input.txt')
+  assert 4 == day7.main.bags_possibly_containing_gold_bag(bag_contents)
+  assert 32 == day7.main.bags_inside_gold_bag(bag_contents)
