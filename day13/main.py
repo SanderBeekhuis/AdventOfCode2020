3df4d59ba74f600988230e81c3457ee15b7c9603
Sander <Sander.Beekhuis@ordina.nl>
2021-01-25 16:45:26 +0100

    Day 13 Part2

--- day13/main.py
+++ day13/main.py
@@ -1,3 +1,6 @@
+import sympy.ntheory.modular
+
+
 def run(file: str = 'input.txt'):
     lines = open(file, 'r').readlines()
     lines = map(lambda l: l.strip(), lines)
@@ -5,16 +8,40 @@ def run(file: str = 'input.txt'):
 
     time = int(lines[0])
     busses = lines[1].split(',')
-    busses = [int(b) for b in busses if b != 'x']
+    known_busses = [int(b) for b in busses if b != 'x']
 
-    wait_times = [b - (time % b) for b in busses]
-    print(busses)
-    print(wait_times)
+    wait_times = [b - (time % b) for b in known_busses]
 
     wait_time = min(wait_times)
-    bus_id = busses[wait_times.index(wait_time)]
+    bus_id = known_busses[wait_times.index(wait_time)]
 
     print("Part1: ", wait_time * bus_id)
+
+    print("Part2: ", part_2(busses))
+
+
+def part_2(busses):
+    """Use sympy's Chines reaminder theorem implementation to solve a system of equations
+
+    time + offset_i = 0 mod bus_id
+
+    We rewite this to the expected form of:
+    time = v[i] mod m[i]
+    like:
+    time = -offset_i mod bus_id
+    """
+
+    m = []
+    v = []
+    offset = -1
+    for bus in busses:
+        offset += 1
+        if bus == 'x':
+            continue
+        m.append(int(bus))
+        v.append(-offset)
+    time, _ = sympy.ntheory.modular.crt(m, v)
+    return time
 
 
 if __name__ == "__main__":
---
+++ day13/test_main.py
@@ -0,0 +1,5 @@
+from day13.main import part_2
+
+
+def test_part_2():
+    assert part_2(['17', 'x', '13', '19']) == 3417
