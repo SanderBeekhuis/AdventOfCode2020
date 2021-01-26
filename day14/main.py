3ed08788f337511d044183fa01c34d4371ef6e8f
Sander <Sander.Beekhuis@ordina.nl>
2021-01-26 12:27:00 +0100

    Day 14, part2

--- day14/main.py
+++ day14/main.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 def run(file: str = 'input.txt'):
     lines = open(file, 'r').readlines()
     lines = map(lambda l: l.strip(), lines)
@@ -17,12 +20,36 @@ def run(file: str = 'input.txt'):
 
     print(f'Part 1: {sum(mem.values())}')
 
+    mem = {}
+    masks = 0, []
+    for line in lines:
+        cmd, arg = line.split(' = ')
+
+        if cmd == 'mask':
+            masks = compute_part2_masks(arg)
+        else:
+            index = int(cmd[4:-1]) | masks[0]
+            for float_mask in masks[1]:
+                mem[index ^ float_mask] = int(arg)
+
+    print(f'Part 2: {sum(mem.values())}')
+
 
 def compute_masks(arg):
     ones_mask = int(arg.replace('X', '0'), base=2)
     zeros_mask = int(arg.replace('1', 'X').replace('0', '1').replace('X', '0'), base=2)
 
     return ones_mask, zeros_mask
+
+
+def compute_part2_masks(arg):
+    ones_mask = int(arg.replace('X', '0'), base=2)
+
+    float_values = [2 ** i for i, arg in zip(reversed(range(36)), arg) if arg == 'X']
+    permutations = itertools.product([True, False], repeat=len(float_values))
+    float_masks = [sum([t for p, t in zip(per, float_values) if p]) for per in permutations]
+
+    return ones_mask, float_masks
 
 
 def mask(x, ones_mask, zeros_mask):
--- day14/test_main.py
+++ day14/test_main.py
@@ -11,5 +11,12 @@ import day14.main
 def test_mask(x, expectation):
     assert day14.main.mask(x, ones_mask=64, zeros_mask=2) == expectation
 
+
 def test_compute_masks():
-    assert day14.main.compute_masks('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X') == (64 ,2)
+    assert day14.main.compute_masks('XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X') == (64, 2)
+
+
+def test_compute_part2_masks():
+    ones_mask, float_masks =  day14.main.compute_part2_masks('000000000000000000000000000000X1001X')
+    assert ones_mask == 18
+    assert float_masks == [33, 32, 1, 0]
