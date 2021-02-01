7e1ac88baae9c0b142ff0d8302519aacf17f6147
Sander <Sander.Beekhuis@ordina.nl>
2021-02-01 14:48:05 +0100

    Day17 Part1: Finish

--- day17/main.py
+++ day17/main.py
@@ -1,3 +1,4 @@
+import itertools
 from collections import defaultdict
 
 
@@ -6,6 +7,9 @@ def run(file: str = 'input.txt'):
     lines = [line.strip() for line in lines]
 
     cg = ConwayGrid(lines)
+    for _ in range(6):
+        cg.update_generation()
+    print(f'Part 1: {cg.active_count()}')
 
 
 class ConwayGrid:
@@ -14,15 +18,40 @@ class ConwayGrid:
         self.z_bounds = range(1)
         self.y_bounds = range(len(lines))
         self.x_bounds = range(len(lines[0]))
-        self.is_active = defaultdict(lambda _ : False) # Grid defaults as inactive
+        self.is_active = defaultdict(lambda: False)  # Grid defaults as inactive
 
-        for x in self.x_bounds:
-            for y in self.y_bounds:
-                self.is_active[x, y, 0] = lines[y][x] == '#'
+        for x, y in itertools.product(self.x_bounds, self.y_bounds):
+            self.is_active[x, y, 0] = lines[y][x] == '#'
 
     def active_count(self):
         # Abuse that True=1 and False=0
         return sum(self.is_active.values())
+
+    def update_generation(self):
+        self.x_bounds = expand_range(self.x_bounds)
+        self.y_bounds = expand_range(self.y_bounds)
+        self.z_bounds = expand_range(self.z_bounds)
+
+        new_is_active = defaultdict(lambda: False)
+
+        for x, y, z in itertools.product(self.x_bounds, self.y_bounds, self.z_bounds):
+            neighbours = list(filter(lambda n: n != (x, y, z),
+                                     itertools.product(range(x - 1, x + 2), range(y - 1, y + 2), range(z - 1, z + 2))))
+
+            active_neighbours = sum(self.is_active[n] for n in neighbours)
+            new_is_active[x, y, z] = active_neighbours == 3 or (self.is_active[x, y, z] and active_neighbours == 2)
+
+        self.is_active = new_is_active
+
+
+def expand_range(r):
+    """
+    Returns a range that's 1 larger on both sides
+
+    >>> expand_range(range(0, 2))
+    range(-1, 3)
+    """
+    return range(min(r) - 1, max(r) + 2)
 
 
 if __name__ == "__main__":
--- day17/test_main.py
+++ day17/test_main.py
@@ -1,12 +1,24 @@
 from day17.main import ConwayGrid
 
-import doctest
+import  pytest
 
-def load_tests(loader, tests, ignore):
-    tests.addTests(doctest.DocTestSuite(day17.main))
-    return tests
+@pytest.fixture
+def cg():
+    """"
+    Creates basic ConwayGrid
+    """
+    return ConwayGrid(['.#.', '..#', '###'])
 
-def test_active_count():
-    cg = ConwayGrid(['.#.', '..#', '###'])
+
+def test_active_count(cg):
     assert cg.active_count() == 5
 
+
+def test_update_generation(cg):
+    cg.update_generation()
+    assert cg.active_count() == 11
+
+    for _ in range(5):
+        cg.update_generation()
+
+    assert cg.active_count() == 112
